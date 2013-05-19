1acf8b299501260ef83f15792a3b4f24548fc80f
mwkuster <marc@budabe.eu>
2013-05-19 09:56:47 +0200

    Added example of two corrigenda published on the same day

--- README.md
+++ README.md
@@ -42,14 +42,30 @@ sudo gem1.9 install sinatra thin
 ```
 sudo gem1.9 install json
 ```
-- 4store as server backend
 
-Configuration
-=============
+- Rest-client
+```
+gem1.9 install rest-client
+```
+
+
+Running of server
+===================
+
 In order to ensure sufficiently long-living connections, start thin directly with a suitable timeout (the default timeout of 30 seconds is insufficient):
 
 ```
 thin -d -t 120 -p 4567 start
 ```
 
+4store as server backend
+===========================
+Largely inspired by the excellent http://www.jenitennison.com/blog/node/152
 
+```
+4s-backend-setup eli (only once during the initial setup)
+4s-backend eli
+4s-httpd -p 8000 -U eli
+```
+
+Data physically stored under /var/lib/4store/eli/
--- test/eli_test.rb
+++ test/eli_test.rb
@@ -20,6 +20,8 @@ class TestSampleELI < Test::Unit::TestCase
     assert_equal("http://eli.budabe.eu/eli/reg/2010/178/corr-fra/2012-01-11/oj", Eli.new("http://publications.europa.eu/resource/celex/32010R0178R%2801%29").eli)  #Celex is 32010R0178R(01)
     assert_equal("http://eli.budabe.eu/eli/reg/2010/178/corr-fra/2012-01-11/oj", Eli.new("http://publications.europa.eu/resource/oj/JOL_2012_007_R_0011_01_REG_2010_178_11").eli)
     assert_equal("http://eli.budabe.eu/eli/dir/2012/12/corr-bul-ces-dan-deu-ell-eng-est-fin-fra-hun-ita-lav-lit-mlt-nld-pol-por-ron-slk-slv-spa-swe/2013-01-31/oj", Eli.new("http://publications.europa.eu/resource/celex/32012L0012R%2801%29").eli)
+    assert_equal("http://eli.budabe.eu/eli/dir/2003/98/corr-fin-por/2011-09-23/oj", Eli.new("http://publications.europa.eu/resource/celex/32003L0098R%2802%29").eli)
+    assert_equal("http://eli.budabe.eu/eli/dir/2003/98/corr-lav-spa/2011-09-23/oj", Eli.new("http://publications.europa.eu/resource/celex/32003L0098R%2803%29").eli)
   end
 end
 
