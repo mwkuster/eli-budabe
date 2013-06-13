7c3460c590b7c9045194ab8fd0941582b617f5f3
mwkuster <marc@budabe.eu>
2013-06-13 06:14:27 +0200

    find celex query adapted

--- cachedrepo.rb
+++ cachedrepo.rb
@@ -83,8 +83,8 @@ SELECT DISTINCT ?celex
 WHERE {
 GRAPH ?g {
   ?manif cdm:manifestation_official-journal_part_information_number ?number .
-  ?manif cdm:manifestation_official-journal_part_typedoc_printer "#{typedoc}"^^xsd:string .
-  ?manif cdm:manifestation_official-journal_part_is_corrigendum_printer "O"^^xsd:string .
+  ?manif cdm:manifestation_official-journal_part_typedoc_printer "#{typedoc}" . 
+  ?manif cdm:manifestation_official-journal_part_is_corrigendum_printer "O"  .
   ?work cdm:resource_legal_id_celex ?celex .
   FILTER(strlen(?number) > 0 && (regex(?number, "^#{year}/#{natural_number}$", "i") || regex(?number, "^#{natural_number}/#{year}$", "i") || regex(?number, "^#{year}/#{natural_number} ", "i") || regex(?number, "^#{natural_number}/#{year} ", "i")))
  }
